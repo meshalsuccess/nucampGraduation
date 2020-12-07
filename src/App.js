3bf0ed7aa651104cbf0eaf3db144f9f699cf4b59
meshalsuccess <meshalsuccess@gmail.com>
2020-12-07 00:12:21 -0800

    adding style to Banner

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
   return (
     <div className="App">
       {/* Nav */}
-
-      {/* Banner */}
       <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
-      isLargeRow/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -47,6 +47,7 @@ function Banner() {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className="banner_fadeBottm"/>
     </header>
   );
 }
