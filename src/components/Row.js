dff945d25ca8cb4323785d0548e30be242f377fc
meshalsuccess <meshalsuccess@gmail.com>
2020-12-06 22:04:38 -0800

    making the images appear on the application, adding image container and base_url from documentation

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../axios';
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import './Row.css';
 /* the reason I started with rows not columns is due to the design of Netflix
   if we take a look at Netflix app, we see that it is in rows and due to that it is extremely
   difficult to design it other wise. Alsom we can see that all the rows are the same but with different
   content and we will use props to deal with that.
 */
 
+//This is from the movie database api documentation :: here is the url
+// https://developers.themoviedb.org/3/getting-started/images
+const base_url = "https://image.tmdb.org/t/p/original";
 
-const Row =({title, fetchUrl}) => {
-
-  const[movies, setMovies] = useState([]);
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
 
   // I need a snippet of code which runs based of a specific condition using useEffect
   useEffect(() => {
@@ -17,21 +20,34 @@ const Row =({title, fetchUrl}) => {
     //if we add movies inside it, it will keep running as long as the movies are changing
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      console.log(request.data.results);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+    //fetchUrl should be included in the array otherwise it won't render again...
+  }, [fetchUrl]);
+
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
       {/* title */}
-  <h2>{title}</h2>
-
-      {/* container -->posters */}
-
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {/* container -->posters */}
+        {movies.map((movie) => (
+          <img
+            className="image"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+          //poster_path is the link that should come after the base_url, check the documentation
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Row;
 
