93eb90c74300dd2220adcde088121f1ccfd7423d
meshalsuccess <meshalsuccess@gmail.com>
2020-12-07 17:23:29 -0800

    adding movie trailers from youtube using react-youtube and movie-trailer packages

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "./Row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 /* the reason I started with rows not columns is due to the design of Netflix
   if we take a look at Netflix app, we see that it is in rows and due to that it is extremely
   difficult to design it other wise. Alsom we can see that all the rows are the same but with different
@@ -13,7 +16,7 @@ const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
   // I need a snippet of code which runs based of a specific condition using useEffect
   useEffect(() => {
     //if we leave the array blank, the code will run once only and does not run again
@@ -28,6 +31,27 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     //fetchUrl should be included in the array otherwise it won't render again...
   }, [fetchUrl]);
 
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'))
+      })
+      .catch(err => console.log(err))
+    }
+  };
  // console.table(movies);
 
   return (
@@ -39,6 +63,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick = {() => handleClick(movie)}
             className={`image ${isLargeRow && "image_large"}`}
             //if the image isLargeRow use image_large else image
             src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`}
@@ -48,6 +73,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
           //poster_path is the link that should come after the base_url, check the documentation
         ))}
       </div>
+      {trailerUrl && <YouTube videoId= {trailerUrl} opts={opts}/>}
     </div>
   );
 };
